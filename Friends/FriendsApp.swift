e8e36b31ab90019747c05b4cfccad19269a27aa1
rishi-singh26 <rishisingh0831@gmail.com>
2023-09-17 12:19:09 +0530

    Initial Commit

---
+++ Friends/FriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsApp.swift
+//  Friends
+//
+//  Created by Rishi Singh on 17/09/23.
+//
+
+import SwiftUI
+
+@main
+struct FriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
